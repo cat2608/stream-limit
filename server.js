8194494245fe9d36adc59224ade6511ebc3e4650
cat2608 <cat2608@gmail.com>
2019-06-07 07:17:40 +0100

    Setup libraries

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')({ level: 'debug' });
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(pino);
 
 app.get('/health', (req, res) => {
   res.json({ message: 'Alive.' });
