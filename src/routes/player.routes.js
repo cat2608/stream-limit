086069b802f9b35c0ed6cd0e412471b47d4ab4ca
cat2608 <cat2608@gmail.com>
2019-06-08 07:41:04 +0100

    Add middlewares

--- src/routes/player.routes.js
+++ src/routes/player.routes.js
@@ -1,5 +1,6 @@
 const player = require('../controller/player.controller');
+const { addToPlaybackList, isPlaylistLimit } = require('../middleware');
 
 module.exports = (app) => {
-  app.get('/api/play/:clipId', player.playClip);
+  app.get('/api/play/:clipId', [addToPlaybackList, isPlaylistLimit], player.playClip);
 };
