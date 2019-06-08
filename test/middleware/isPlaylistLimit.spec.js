1cc745a2fdd1212ca8c7fddd653ffc1b74e91a67
Catalina <cat2608@gmail.com>
2019-06-08 07:19:18 +0100

    Naming refactor (#1)

--- test/middleware/isPlaylistLimit.spec.js
+++ test/middleware/isPlaylistLimit.spec.js
@@ -6,7 +6,7 @@ const { expect } = chai;
 
 chai.use(sinonChai);
 
-const isPlaylistFull = require('../../src/middleware/isPlaylistFull.middleware');
+const isPlaylistLimit = require('../../src/middleware/isPlaylistLimit.middleware');
 
 describe('Check open connections', () => {
   const res = {};
@@ -17,18 +17,18 @@ describe('Check open connections', () => {
   });
 
   it('should fail when user has more than 3 open connections', () => {
-    res.locals = { auth: { openConnection: 4 } };
+    res.locals = { auth: { openConnections: 4 } };
 
-    isPlaylistFull({}, res, () => {});
+    isPlaylistLimit({}, res, () => {});
     expect(res.status).to.have.been.calledWith(403);
   });
 
   it('should call next middleware when user has up to 3 connections', () => {
-    res.locals = { auth: { openConnection: 2 } };
+    res.locals = { auth: { openConnections: 2 } };
 
     const next = sinon.stub();
 
-    isPlaylistFull({}, res, next);
+    isPlaylistLimit({}, res, next);
     expect(next).to.have.been.called;
   });
 });
